140065e849c6a258d87bd0d28999ea9af0ad8b1a
Ilja Robalkin <ilja.robalkin@r8tech.io>
2025-08-19 12:25:53 +0300

    fix: repair code broken by copilot

--- lesson-08/main.go
+++ lesson-08/main.go
@@ -20,45 +20,47 @@ func main() {
 			oddNumbers = append(oddNumbers, i)
 		}
 	}
+
 	startTime := time.Now()
 	fmt.Println("Starting to print numbers...")
 	vg.Add(2)
 	go printNumbers(evenNumbers, "Even:", &vg)
 	go printNumbers(oddNumbers, "Odd:", &vg)
-
 	vg.Wait()
-	fmt.Println("All numbers printed.")
+
+	fmt.Println("All numbers printed!")
 	elapsedTimeTask1 := time.Since(startTime).Milliseconds()
-	fmt.Println("Elapsed time in milliseconds:", elapsedTimeTask1)
+	fmt.Printf("Elapsed time %d milliseconds\n", elapsedTimeTask1)
 	fmt.Println("-----------------------------------")
 
+	var vgFiles sync.WaitGroup
 	startTime = time.Now()
 	fmt.Println("Starting to load files...")
-	vg.Add(3)
-	go loadFile("file1.txt", time.Second, &vg)
-	go loadFile("file2.txt", 2*time.Second, &vg)
-	go loadFile("file3.txt", 500*time.Millisecond, &vg)
 	vgFiles.Add(3)
 	go loadFile("file1.txt", time.Second, &vgFiles)
 	go loadFile("file2.txt", 2*time.Second, &vgFiles)
 	go loadFile("file3.txt", 500*time.Millisecond, &vgFiles)
 	vgFiles.Wait()
-	fmt.Println("All files loaded.")
+
+	fmt.Println("All files loaded!")
 	elapsedTimeTask2 := strconv.FormatFloat(time.Since(startTime).Seconds(), 'f', 3, 64)
-	fmt.Println("Elapsed time in seconds:", elapsedTimeTask2)
+	fmt.Printf("Total elapsed time to load files: %s seconds\n", elapsedTimeTask2)
 }
 
 func printNumbers(numbers []int, prefix string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for _, number := range numbers {
 		fmt.Println(prefix + strconv.Itoa(number))
-		time.Sleep(time.Millisecond) // Simulate some work
+		time.Sleep(time.Millisecond) // Simulate some work, otherwise one goroutine ends before the other
 	}
 }
 
+// Not an actual file loading, just an "imitation"
 func loadFile(fileName string, waitTime time.Duration, wg *sync.WaitGroup) {
 	defer wg.Done()
+	startTime := time.Now()
 	fmt.Println("Loading file:", fileName)
 	time.Sleep(waitTime)
-	fmt.Println("File loaded:", fileName)
+	elapsedTime := strconv.FormatFloat(time.Since(startTime).Seconds(), 'f', 3, 64)
+	fmt.Printf("File %s loaded in %s seconds\n", fileName, elapsedTime)
 }
