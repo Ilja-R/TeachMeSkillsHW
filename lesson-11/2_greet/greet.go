ef7f76b31cc5cc1cb6f808c41d47c02d9bcc13cd
ilja.robalkin <ilja.robalkin@taltech.ee>
2025-08-31 20:19:00 +0300

    feat: homework 11

---
+++ lesson-11/2_greet/greet.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type Greeting struct {
+	Name string `json:"name"`
+}
+
+type GreetingResponse struct {
+	Message string `json:"message"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	var g Greeting
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	w.Header().Set("Content-Type", "application/json")
+	if err := dec.Decode(&g); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(ErrorResponse{err.Error()})
+		return
+	}
+	if strings.TrimSpace(g.Name) == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(ErrorResponse{Error: "name is required"})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(GreetingResponse{fmt.Sprintf("Hello, %s!", g.Name)}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(ErrorResponse{err.Error()})
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/greet", greetHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
