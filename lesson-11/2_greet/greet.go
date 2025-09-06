3ded70e733c9fd7303e9bbc45e17b42d187de191
iljarobalkin <robalkin.ilja@gmail.com>
2025-09-06 19:47:24 +0300

    feat: homework 12

--- lesson-11/2_greet/greet.go
+++ lesson-11/2_greet/greet.go
@@ -20,6 +20,10 @@ type ErrorResponse struct {
 }
 
 func greetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed!", http.StatusMethodNotAllowed)
+		return
+	}
 	var g Greeting
 	dec := json.NewDecoder(r.Body)
 	dec.DisallowUnknownFields()
